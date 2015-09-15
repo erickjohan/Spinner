2c4a7f1cda5631dc7f113fd3391ac9b4853192a9
toshiba-pc <erickfather96@gmail.com>
2015-09-15 08:02:00 -0700

    Spinner Finalizado

---
+++ app/src/main/java/com/example/toshiba_pc/spinner/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.toshiba_pc.spinner;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends Activity {
+
+    android.widget.Spinner spinner;
+    String[] spinnerlista = {"Seleccionar", "Item 1", "Item 2", "Item 3", "Item 4", "Item 5"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = (android.widget.Spinner)findViewById(R.id.SpinneR);
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerlista);
+        spinner.setAdapter(adaptador);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
